41062c389b8af9c95eaaaa1849e10fc21393ebb5
Tareq Hassan <tareqhassan2014@gmail.com>
2023-11-12 22:49:07 +0600

    add: complete cpp module 1

--- c++programming/module-1/tempCodeRunnerFile.cpp
+++ c++programming/module-1/tempCodeRunnerFile.cpp
@@ -1,1 +1,10 @@
-#include <iostream>
+  // int a = 13;
+
+    // if (a == 10)
+    // {
+    //     cout << "it is ten." << endl;
+    // }
+    // else
+    // {
+    //     cout << "It is not ten." << endl;
+    // }
