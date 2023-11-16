1583ec6c3bf815db526cdf3aae66bd2f84788fbd
Tareq Hassan <tareqhassan2014@gmail.com>
2023-11-16 19:58:05 +0600

    smaller pair

--- module-3.5/04-smallest-pair.cpp
+++ module-3.5/04-smallest-pair.cpp
@@ -4,17 +4,33 @@ using namespace std;
 int main()
 {
 
-    int n;
-    cin >> n;
+    int t;
+    cin >> t;
 
-    int ar[n];
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < t; i++)
     {
-        cin >> ar[i];
-    }
-    for (int i = 0; i < n; i++)
-    {
-        cout << ar[i];
+
+        int n;
+        cin >> n;
+
+        int ar[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> ar[i];
+        }
+
+        int mn_sum = INT_MAX;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                int ans = ar[i] + ar[j] + j - i;
+                int mn_sum = min(mn_sum, ans);
+            }
+        }
+
+        cout << mn_sum << endl;
     }
 
     return 0;
