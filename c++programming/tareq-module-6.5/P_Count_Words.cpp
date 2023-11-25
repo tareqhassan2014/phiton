871c97ac63dcbcb7b5400c716568138285771663
Tareq Hassan <tareqhassan2014@gmail.com>
2023-11-25 10:33:06 +0600

    add problem

--- c++programming/tareq-module-6.5/P_Count_Words.cpp
+++ c++programming/tareq-module-6.5/P_Count_Words.cpp
@@ -3,42 +3,6 @@ using namespace std;
 
 int main()
 {
-
-    string str;
-
-    getline(cin, str);
-
-    int count = 0;
-    bool new_word = false;
-
-    for (char c : str)
-    {
-
-        if (isalpha(c))
-        {
-            if (!new_word)
-            {
-                count++;
-                new_word == true;
-            }
-        }
-        else
-        {
-            new_word == false;
-        }
-    }
-
-    cout << count << endl;
-
-    return 0;
-}
-
-/*
-#include <bits/stdc++.h>
-using namespace std;
-
-int main()
-{
     string str;
     getline(cin, str);
 
@@ -65,4 +29,3 @@ int main()
 
     return 0;
 }
- */
