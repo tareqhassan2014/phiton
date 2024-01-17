ec2e6c3ddaa615bac73c4afcdd45c902e628e99f
Tareq Hassan <tareqhassan2014@gmail.com>
2024-01-17 22:23:47 +0600

    Refactor word counting logic and add support for multiple test cases

--- Count_Me.cpp
+++ Count_Me.cpp
@@ -3,24 +3,42 @@ using namespace std;
 
 int main()
 {
+    int test_cases;
 
-    string sentence;
-    getline(cin, sentence);
+    cin >> test_cases;
 
-    string word;
-    stringstream ss(sentence);
+    cin.ignore();
 
-    map<string, int> mp;
-
-    while (ss >> word)
+    while (test_cases--)
     {
+        string sentence;
 
-        mp[word]++;
-    }
+        getline(cin, sentence);
 
-    for (auto it = mp.begin(); it != mp.end(); it++)
-    {
-        cout << it->first << " " << it->second << " " << endl;
+        if (sentence.empty())
+            continue;
+
+        string word;
+        stringstream ss(sentence);
+
+        map<string, int> word_count;
+
+        int max_frequency = 0;
+
+        string most_frequent_word = "";
+
+        while (ss >> word)
+        {
+            word_count[word]++;
+
+            if (word_count[word] > max_frequency)
+            {
+                max_frequency = word_count[word];
+                most_frequent_word = word;
+            }
+        }
+
+        cout << most_frequent_word << " " << max_frequency << endl;
     }
 
     return 0;
